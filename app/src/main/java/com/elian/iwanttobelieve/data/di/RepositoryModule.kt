0bf3cf1cf2fb09af4d918f32997de68e5eceac6f
Elian Antunes <elian.antunes09@gmail.com>
2025-08-18 17:00:00 -0300

    feat(di): provide user repository instance with hilt

---
+++ app/src/main/java/com/elian/iwanttobelieve/data/di/RepositoryModule.kt
@@ -0,0 +1,26 @@
+package com.elian.iwanttobelieve.data.di
+
+import com.elian.iwanttobelieve.data.datasource.PostRemoteDataSource
+import com.elian.iwanttobelieve.data.datasource.UserAuthDataSource
+import com.elian.iwanttobelieve.data.datasource.UserRemoteDataSource
+import com.elian.iwanttobelieve.data.repository.PostRepository
+import com.elian.iwanttobelieve.data.repository.UserRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(
+        authDataSource: UserAuthDataSource,
+        remoteDataSource: UserRemoteDataSource
+    ): UserRepository {
+        return UserRepository(authDataSource, remoteDataSource)
+    }
+}
